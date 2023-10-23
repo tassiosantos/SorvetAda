f4b7f34cf0a6b1bb00bcad1449fe6f8e20d461fa
tassiosantos <tassiosantos@gmail.com>
2023-10-23 18:52:52 -0300

    Inicial2

---
+++ src/main/java/com/ada/sorvetada/SorvetadaApplication.java
@@ -0,0 +1,13 @@
+package com.ada.sorvetada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorvetadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SorvetadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ada/sorvetada/SorvetadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.sorvetada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorvetadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
