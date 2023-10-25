5b156cadfd4ec1e3e935e5c448c892788861be9f
Adir Silva Filho <tarukagahobenu@gmail.com>
2023-10-25 20:56:52 -0300

    feat: create import.sql with basic data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+-- IceCreams
+INSERT INTO public.icecreams(unit_price, id, name) VALUES (3.25, 1, 'Chocolate');
+INSERT INTO public.icecreams(unit_price, id, name) VALUES (2.75, 2, 'Creme');
+INSERT INTO public.icecreams(unit_price, id, name) VALUES (4.20, 3, 'Cannabis');
+INSERT INTO public.icecreams(unit_price, id, name) VALUES (5.15, 4, 'Dois amores - metade creme, metade chocolate');
+
+-- Customers
+INSERT INTO public.customer(birth_date, id, address, cpf, email, name, password) VALUES ('1980-07-25', 1, 'Qualquer coisa', 1, 'teste@email.com', 'Teste 01', '123456');
